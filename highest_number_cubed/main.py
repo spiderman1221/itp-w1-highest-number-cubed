b95b89ea903077ea14d00e4eeb0a848cd65f5ed2
Michael Netherland <michaeljnetherland@gmail.com>
2017-02-17 01:39:32 +0000

    initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,12 @@
 """This is the entry point of the program."""
-
+#highest cubable number still less than argument
 
 def highest_number_cubed(limit):
-    pass
+    y=0
+    for i in range(limit):
+        while (y**3) <= limit:
+            y += 1
+    return (y -1)        
+        
+    
+    
